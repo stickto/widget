973325a0502bf336f7c2008a81b578724d7f2d68
毋愁 <jianhua.wjh@alibaba-inc.com>
2020-07-18 16:23:35 +0800

    add config

---
+++ src/model/widget/instance/WidgetConfig.ts
@@ -0,0 +1,7 @@
+import WidgetLayout from './WidgetLayout';
+
+export default class WidgetConfig {
+  fieldValues: Object<string, string>;
+
+  layout: WidgetLayout;
+}
---
+++ src/model/widget/instance/WidgetLayout.ts
@@ -0,0 +1,20 @@
+export default class WidgetLayout {
+  x: number;
+
+  y: number;
+
+  width: number;
+
+  height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  static fromObject(obj: any) {
+    return new WidgetLayout(obj.x, obj.y, obj.width, obj.height);
+  }
+}
