e686df6d34cac2a77bbc14a57c2e1254ed72e756
毋愁 <jianhua.wjh@alibaba-inc.com>
2020-07-20 21:12:36 +0800

    init share

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import { Provider } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import store from './reducers';
 import Home from './ui/Home';
+import Share from './ui/Share';
 import './App.css';
 
 const App: FC = () => (
   <Provider store={store}>
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/share" exact component={Share} />
+    </Router>
   </Provider>
 );
 
---
+++ src/ui/Share/Share.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const Share: FC = () => {
+  const i = 0;
+  return (
+    <div>ahah</div>
+  );
+};
+export default Share;
